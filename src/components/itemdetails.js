1ad0dbb9b17794a42df01774df9a87fa34a99b7c
Jazzmin Johnson <jazzminceleste@gmail.com>
2024-01-25 12:56:02 -0900

    3 has js file with data

---
+++ src/components/itemdetails.js
@@ -0,0 +1,159 @@
+const items = [
+  {
+    category: 'Signature Burgers',
+    name: 'Animal Style',
+    description: 'Grilled onions, house fry sauce, Cheddar cheeze, lettuce, pickles and tomato.',
+    price: '$17.00'
+  },
+  {
+    category: 'Signature Burgers',
+    name: 'Western',
+    description: 'Beyond patty slathered in house BBQ sauce, crispy onions, coleslaw, garlic aioli and petite diced onions.',
+    price: '$18.00'
+  },
+  {
+    category: 'Signature Burgers',
+    name: 'Big Mac',
+    description: 'Double patty, cheddar, petite diced onions, garlic aioli, fry sauce, lettuce, pickle and tomato with an extra bun inside.',
+    price: '$18.00'
+  },
+  {
+    category: 'Signature Burgers',
+    name: 'Mushroom Swiss',
+    description: 'Sautéed mushrooms, Swiss cheeze, pickles, petite diced onions, lettuce and tomato',
+    price: '$18.00'
+  },
+  {
+    category: 'Signature Burgers',
+    name: "Crispy Chick'n Sandwich",
+    description: "Crispy chick'n patty, lettuce, tomato, pickle and garlic aioli.",
+    price: '$17.00'
+  },
+  {
+    category: 'Signature Burgers',
+    name: 'Jalapeno Avocado Swiss',
+    description: 'Garlic aioli, onions, pickles, tomatoes, lettuce, grilled jalapenos, fresh avocado and melted swiss',
+    price: '$18.00'
+  },
+  {
+    category: 'Signature Burgers',
+    name: 'Firebreather Burger',
+    description: 'Grilled poblanos over a beyond patty with melty swiss chz and bang sauce. Has lettuce, tomato, onion, pickle.',
+    price: '$18.00'
+  },
+  {
+    category: 'Signature Burgers',
+    name: 'Cheddar Jalapeno Popper Burger',
+    description: 'Hand breaded and deep fried fire roasted jalapeno halves ontop a cheddar cheeze beyond patty. Served with lettuce tomato onion pickles and house ranch sauce.',
+    price: '$18.00'
+  },
+  {
+    category: "Panini's",
+    name: 'Turkee Pesto',
+    description: 'Local! Northern Seitans Turkee, Pesto, Swiss, Tomato, Spinach, Arugula.',
+    price: '$18.50'
+  },
+  {
+    category: "Panini's",
+    name: 'Memphis BBQ Beef',
+    description: 'Beyond Beef tossed in our house Memphis BBQ, Swiss Cheeze and diced onion!',
+    price: '$18.50'
+  },
+  {
+    category: "Philly's",
+    name: 'Build Your Own Philly!',
+    description: 'Each Philly comes automatically with sautéed onions, mushrooms, Beyond Beef and soy free Mozzarella.',
+    price: '$17.00'
+  },
+  {
+    category: 'Spinach Wraps',
+    name: "Garlic Buffalo Chick'n Wrap",
+    description: "In house breaded seitan chick'n tossed in our garlic buffalo sauce, lettuce, tomato, ranch.",
+    price: '$17.00'
+  },
+  {
+    category: 'Spinach Wraps',
+    name: 'Hummus Wrap',
+    description: 'Cucumber, Tomato, Spinach, Arugula, Chard, Hummus.',
+    price: '$14.50'
+  },
+  {
+    category: "Po Boy's",
+    name: 'Classic',
+    description: "Hand breaded chick'n pieces, lettuce, tomato, garlic aioli.",
+    price: '$14.40'
+  },
+  {
+    category: "Po Boy's",
+    name: "Crispy Cajun Chick'n",
+    description: "Hand breaded chick'n tossed in Cajun seasoning with creole slaw, tomato, and remoulade sauce.",
+    price: '$14.40'
+  },
+  {
+    category: "Po Boy's",
+    name: "Buffalo Chick'n",
+    description: "Hand breaded chick'n tossed in buffalo sauce with lettuce, tomato and garlic aioli.",
+    price: '$14.40'
+  },
+  {
+    category: 'Sides',
+    name: 'Classic Fries',
+    description: 'Crispy straight cut Idaho russet.',
+    price: '$5.50'
+  },
+  {
+    category: 'Sides',
+    name: 'Animal Style Fries',
+    description: 'Melted cheeze, sautéed onions and house fry sauce.',
+    price: '$7.80'
+  },
+  {
+    category: 'Sides',
+    name: 'Philly Fries',
+    description: 'Melted cheeze, peppers, onions, mushroom and beyond.',
+    price: '$7.80'
+  },
+  {
+    category: 'Sides',
+    name: 'Onion Rings',
+    description: 'Hand cut, battered and breaded organic yellow onions. Tossed in garlic rub and served with ranch.',
+    price: '$12.50'
+  },
+  {
+    category: 'Sides',
+    name: 'Pickle Chips',
+    description: 'Thick cut Dill Pickles battered, breaded and deep fried. Seasoned up and served with ranch.',
+    price: '$12.50'
+  },
+  {
+    category: 'Sides',
+    name: "Buffalo Chik'n Ranch Fries",
+    description: 'Chopped Seitan chx tossed in buffalo and topped with ranch',
+    price: '$7.80'
+  },
+  {
+    category: 'Greek',
+    name: 'Beyond Gyro',
+    description: 'Warm pita stuffed with beyond beef, lettuce, tomato, onion, tzatziki and feta',
+    price: '$14.50'
+  },
+  {
+    category: 'Greek',
+    name: 'Portobello Gyro',
+    description: 'Warm pita stuffed with portobello mushroom, lettuce, tomato, onion, tzatziki and feta.',
+    price: '$14.50'
+  },
+  {
+    category: 'Greek',
+    name: "Crispy Chick'n Gyro",
+    description: "Warm pita stuffed with hand breaded seasoned chick'n, lettuce, tomato, onion, tzatziki and feta.",
+    price: '$14.50'
+  },
+  {
+    category: 'Boneless Wyngs',
+    name: '8 PC',
+    description: 'Tossed in sauce or dry rub. Served with ranch and celery sticks',
+    price: '$20.00'
+  },
+
+]
