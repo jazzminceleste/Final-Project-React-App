d3c9665619b6133e5530e2fb4a92740c8f6d2028
Jazzmin Johnson <jazzminceleste@gmail.com>
2024-01-25 15:28:23 -0900

    6 added reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,7 +1,19 @@
 const Reviews = () => {
 
   return (
-    <div></div>
+    <div>
+      <h2>Reviews</h2>
+      <p>
+        Samuel P <hr />
+        7/21/23<hr />
+        The Beyond Gyro is "out of bounds" delicious. The beef was kinda on the salty side, however, the lettuce, feta, onions and taziki balanced it all out. The food here is amazing, light in portion (this place actually implements portion control). Major kudos!
+      </p>
+      <p>
+        Vonda I <hr />
+        4/27/23 <hr />
+        Wow!! I got the Animal Style Fries and the Big Mac, both were super good, flavorful, and fresh. Such a great price for the quality and quantity you get. I look forward to trying the rest of the menu!
+      </p>
+    </div>
   )
 }
 
