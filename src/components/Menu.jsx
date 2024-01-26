6844b9c94944c88b0f8a2f79579f56df62ec6172
Jazzmin Johnson <jazzminceleste@gmail.com>
2024-01-25 16:27:00 -0900

    10 created menu card

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,11 +3,11 @@
 const Menu = (props) => {
 
   return (
-    <div>
+    <div className="menu-card">
       <h2>Menu</h2>
       <h3 className='flex-ctr'>{props.items.category}</h3>
       <h4 className='flex-ctr'>{props.items.name}</h4>
-      <h4 className='flex-ctr'>{props.items.descriptions}</h4>
+      <h4 className='flex-ctr'>{props.items.description}</h4>
       <h5 className='flex-ctr'>{props.items.price}</h5>
     </div>
   )
