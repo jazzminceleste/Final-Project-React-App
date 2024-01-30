35a2e25ec4ae0a1b90f5a557ac2bdcd9078ddac0
Jazzmin Johnson <jazzminceleste@gmail.com>
2024-01-30 12:38:20 -0900

    18 progress

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -4,7 +4,9 @@ const Values = () => {
     <div className="values" >
       <h2>Values</h2>
       "Veganism is a way of living that seeks to exclude, as far as possible and practicable, all forms of exploitation of, and cruelty to, animals for food, clothing and any other purpose."<br />
-      -Donald Watson and The Vegan Society, founded in 1944
+      <div>
+        -Donald Watson and The Vegan Society, founded in 1944
+      </div>
     </div >
   )
 }
