731c2b8e3525c9d3d533367fce13cf2a4cf6a6d8
Jazzmin Johnson <jazzminceleste@gmail.com>
2024-01-30 12:28:33 -0900

    17 cleaning up css

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -13,6 +13,26 @@ const Reviews = () => {
         4/27/23 <br />
         "Wow!! I got the Animal Style Fries and the Big Mac, both were super good, flavorful, and fresh. Such a great price for the quality and quantity you get. I look forward to trying the rest of the menu!"
       </p>
+      <p>
+        Jordan T <br />
+        1/13/22<br />
+        "Always delicious, love having a quality vegan option!"
+      </p>
+      <p>
+        LeeAnna M <br />
+        5/24/23<br />
+        "Preference has been a longtime favorite vegan spot for me. Some of my favorite meals are the Western burger, the Ultimate Breakfast Sandwich, Animal style fries...the portions are perfect, the service is exceptional and the wait times aren't ever very long at all. 12/10 recommend dining in, pickup, or delivery!"
+      </p>
+      <p>
+        Tiffany M <br />
+        7/20/22<br />
+        "Amazing vegan food!! Honestly just amazing food! So proud of Anchorage for having a vegan restaurant"
+      </p>
+      <p>
+        Manny B <br />
+        1/19/22<br />
+        "Vegan comfort food!!"
+      </p>
     </div>
   )
 }
